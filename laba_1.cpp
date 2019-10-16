b6a401b78fc8da4e98295932a74ba911ec7ae3cb
solnce69 <arinadanil4enko@yandex.ru>
2019-10-16 20:02:11 +0300

    func_BinIn

--- laba_1.cpp
+++ laba_1.cpp
@@ -12,3 +12,13 @@ void BinOut(unsigned char* Buf, int len) {
 			_putch(byte & 0x80 ? '1' : '0');
 	printf("\n");
 }
+void BinIn(unsigned char* Buf, int len) {
+	int i, j;
+	char ch;
+	for (i = len - 1; i >= 0; i--)
+		for (j = 1, Buf[i] = 0; j <= 8; Buf[i] = (Buf[i] << 1) + (ch - '0'), j++) {
+			while ((ch = _getch()) != '0' && ch != '1');
+			_putch(ch);
+		}
+	printf("\n");
+}
