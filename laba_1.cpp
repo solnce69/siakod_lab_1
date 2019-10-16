537837e94a476f128288ccad01d372ec9589373e
solnce69 <arinadanil4enko@yandex.ru>
2019-10-16 19:56:46 +0300

    func_BinOut

---
+++ laba_1.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include <iostream>
+#include <iomanip>
+#include <stdio.h> 
+#include <conio.h> 
+using namespace std;
+void BinOut(unsigned char* Buf, int len) {
+	int i, j;
+	unsigned char byte;
+	for (i = len - 1; i >= 0; i--)
+		for (j = 1, byte = Buf[i]; j <= 8; byte <<= 1, j++)
+			_putch(byte & 0x80 ? '1' : '0');
+	printf("\n");
+}
