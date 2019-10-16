fb2fd6cae5156ab4c723d80f6d0d46ea520d36fb
solnce69 <arinadanil4enko@yandex.ru>
2019-10-16 20:04:07 +0300

    func_main

--- laba_1.cpp
+++ laba_1.cpp
@@ -22,3 +22,36 @@ void BinIn(unsigned char* Buf, int len) {
 		}
 	printf("\n");
 }
+int main(int argc, char* argv[]) {
+	int n;
+	cout << "Enter number of a int type: ";
+	cin >> n;
+	cout << "This is its binary representation:" << endl;
+	BinOut((unsigned char*)&n, sizeof(n));
+	cout << "Enter binary representation of a int number:" << endl;
+	BinIn((unsigned char*)&n, sizeof(n));
+	cout << "This is its value: " << n << endl;
+	double y;
+	cout << "Enter number of a double type: ";
+	cin >> y;
+	cout << "This is its binary representation:" << endl;
+	BinOut((unsigned char*)&y, sizeof(y));
+	cout << "Enter binary representation of a double number:" << endl;
+	BinIn((unsigned char*)&y, sizeof(y));
+	cout << "This is its value: " << setprecision(20) << y << endl;
+	typedef unsigned long arr[7];
+	arr A;
+	for (int i = 0; i < 7; i++) {
+		cout << "Enter number of a unsigned long type (A[" << i + 1 << "]): ";
+		cin >> A[i];
+	}
+	BinOut((unsigned char*)&A, sizeof(A));
+	cout << "Enter binary representation of a double number:" << endl;
+	BinIn((unsigned char*)&A, sizeof(A));
+	cout << "This is its value: ";
+	for (int i = 0; i < 7; i++) {
+
+		cout << A[i] << " ";
+	}
+	return 0;
+}
